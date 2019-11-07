f8095a0ebfa6b360ac32fcfaacbeabb14517b3be
morrisrobert <morrisrobert@outlook.com>
2019-11-06 21:20:28 -0600

    Updates

---
+++ precache-manifest.001633ed7247e0bae1160d138b92c8c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef57882f1c759a39c97155bf43cdff1",
+    "url": "/event-finder/index.html"
+  },
+  {
+    "revision": "c4f06b0f9053fc405d8c",
+    "url": "/event-finder/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "f08d6d819c90056b6dd7",
+    "url": "/event-finder/static/css/main.4ca9d24c.chunk.css"
+  },
+  {
+    "revision": "c4f06b0f9053fc405d8c",
+    "url": "/event-finder/static/js/2.947acd5d.chunk.js"
+  },
+  {
+    "revision": "f08d6d819c90056b6dd7",
+    "url": "/event-finder/static/js/main.1d7ab628.chunk.js"
+  },
+  {
+    "revision": "29f2ca452103373d1cd1",
+    "url": "/event-finder/static/js/runtime-main.c30c6f5f.js"
+  },
+  {
+    "revision": "302f7661a7c6737bca99645b41f33e0b",
+    "url": "/event-finder/static/media/concert.302f7661.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/event-finder/static/media/logo.25bf045c.svg"
+  }
+]);
